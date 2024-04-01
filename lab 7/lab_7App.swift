7d678df32c839e39faec503481d6b80a1f842398
Zachary <zach.lee96@gmai.com>
2024-03-31 17:40:41 -0700

    Initial Commit

---
+++ lab 7/lab_7App.swift
@@ -0,0 +1,17 @@
+//
+//  lab_7App.swift
+//  lab 7
+//
+//  Created by Zachary Lee on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct lab_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
